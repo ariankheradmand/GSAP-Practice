46177db2ca5fbff05f6c9b8c331f0ee177466b52
Arian <ariankheradmand@gmail.com>
2025-08-23 01:19:19 +0330

    Hot toast Updated

--- app/HotToast/page.js
+++ app/HotToast/page.js
@@ -1,13 +1,16 @@
-import HotToast from '@/components/HotToast'
-import React from 'react'
+import HotToast from "@/components/HotToast";
+import React from "react";
 
 function page() {
   return (
     <div>
-        <HotToast />
-
+      <HotToast
+        timer={false}
+        type={"success"}
+        content={"Your changes have been saved successfully."}
+      />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
